75b4d546fd651ed79ec1dd8ab40a7eeff2951ff0
Baskoro Adi <baskoroadi.wicaksono@gmail.com>
2022-03-02 11:55:40 +0700

    Register Page Done

---
+++ src/pages/register/Register.jsx
@@ -0,0 +1,54 @@
+import React from 'react'
+import "./register.scss"
+
+const Register = () => {
+    const [email, setEmail] = React.useState('');
+    const [password, setPassword] = React.useState('');
+
+    const emailRef = React.useRef();
+    const passwordRef = React.useRef();
+
+    const handleStart = () => {
+        setEmail(emailRef.current.value);
+    }
+    const handleFinish = () => {
+        setPassword(passwordRef.current.value);
+    }
+
+  return (
+    <div className='register'>
+        <div className='top'>
+            <div className='wrapper'>
+                <img 
+                src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/1198px-Netflix_2015_logo.svg.png?20190206123158" 
+                alt='netflix-logo'
+                className='logo'
+                />
+                <button className='loginButton'>
+                    Sign In
+                </button>
+            </div>
+        </div>
+        <div className="container">
+            <h1>Unlimited movies, TV shows, and more.</h1>
+            <h2>Watch anywhere. Cancel anytime.</h2>
+            <p>Ready to watch? Enter your email to create or restart your membership</p>
+            {!email ? (
+                <div className='input'>
+                    <input type="email" placeholder="Email" ref={(emailRef)} />
+                    <button className='registerButton' onClick={handleStart}>Get Started</button>
+                </div> )
+                : (
+                <form className='input'>
+                    <input type="password" placeholder="Password" ref={(passwordRef)} />
+                    <button className='registerButton' onClick={handleFinish}>Start</button>
+                </form>
+                )
+            }
+            
+        </div>
+    </div>
+  )
+}
+
+export default Register
