9542103903334de953efc531e7baf761cae0ebfb
Baskoro Adi <baskoroadi.wicaksono@gmail.com>
2022-03-07 16:53:20 +0700

    charting done

---
+++ admin/src/dataDummies.js
@@ -0,0 +1,50 @@
+export const data = [
+    {
+      name: 'Jan',
+      "Active User": 4000
+    },
+    {
+      name: 'Feb',
+      "Active User": 3000
+    },
+    {
+      name: 'Mar',
+      "Active User": 2000
+    },
+    {
+      name: 'Apr',
+      "Active User": 2780
+    },
+    {
+      name: 'May',
+      "Active User": 1890
+    },
+    {
+      name: 'Jun',
+      "Active User": 2390
+    },
+    {
+      name: 'Jul',
+      "Active User": 3490
+    },
+    {
+        name: 'Aug',
+        "Active User": 7623
+    },
+    {
+        name: 'Sep',
+        "Active User": 8629
+    },
+    {
+        name: 'Oct',
+        "Active User": 2409
+    },
+    {
+        name: 'Nov',
+        "Active User": 1059
+    },
+    {
+        name: 'Dec',
+        "Active User": 6969
+    }
+  ];
